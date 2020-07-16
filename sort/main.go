b012a3d93b8683b170afc8045e1fea599461e548
Irainia <anwarhidayat14@gmail.com>
2020-07-16 12:08:09 +0700

    add main for for selection sort

--- sort/main.go
+++ sort/main.go
@@ -9,10 +9,17 @@ import (
 // Main plays all sorting algorithms
 func Main() {
 	sequence := []int{3, 5, 1, 7, 7, 9, 10, 3}
+	maxSequenceLength := uint(60000)
 
 	fmt.Println("=== Bubble Sort ===")
 	playMiniBubbleSort(sequence)
-	playLargeBubbleSort(100000)
+	playLargeBubbleSort(maxSequenceLength)
+
+	fmt.Println()
+
+	fmt.Println("=== Selection Sort ===")
+	playMiniSelectionSort(sequence)
+	playLargeSelectionSort(maxSequenceLength)
 }
 
 func playMiniBubbleSort(sequence []int) {
@@ -30,6 +37,26 @@ func playLargeBubbleSort(maxSequenceLength uint) {
 
 	startTime := time.Now()
 	BubbleSort(sequence)
+	endTime := time.Now()
+
+	fmt.Printf("sort on %d items takes: %v\n", maxSequenceLength, endTime.Sub(startTime))
+}
+
+func playMiniSelectionSort(sequence []int) {
+	fmt.Printf("before: %+v\n", sequence)
+	SelectionSort(sequence)
+	fmt.Printf("after: %+v\n", sequence)
+}
+
+func playLargeSelectionSort(maxSequenceLength uint) {
+	sequence := make([]int, maxSequenceLength)
+
+	for i := uint(0); i < maxSequenceLength; i++ {
+		sequence[i] = rand.Int()
+	}
+
+	startTime := time.Now()
+	SelectionSort(sequence)
 	endTime := time.Now()
 
 	fmt.Printf("sort on %d items takes: %v\n", maxSequenceLength, endTime.Sub(startTime))

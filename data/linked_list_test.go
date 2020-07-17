3b9b09f85ed947f1df931591cf21317e94ba906f
Irainia <anwarhidayat14@gmail.com>
2020-07-17 17:20:55 +0700

    pass spec for length should be the same as sequence

--- data/linked_list_test.go
+++ data/linked_list_test.go
@@ -19,3 +19,18 @@ func TestNewEmptyLinkedList(t *testing.T) {
 		}
 	})
 }
+
+func TestNewAllocatedLinkedList(t *testing.T) {
+	t.Run("length should be the same as sequence", func(t *testing.T) {
+		sequence := []int{3, 5, 2, 1, 4}
+		linkedList := data.NewAllocatedLinkedList(sequence)
+
+		expectedLength := 5
+
+		actualLength := linkedList.GetLength()
+
+		if expectedLength != actualLength {
+			t.Errorf("expected: %d -- actual: %d", expectedLength, actualLength)
+		}
+	})
+}

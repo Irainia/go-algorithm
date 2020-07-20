b38aef8722897f55c71b579896cee12be69afce0
Irainia <anwarhidayat14@gmail.com>
2020-07-20 14:20:59 +0700

    pass spec for should panic if stack is empty

--- data/stack.go
+++ data/stack.go
@@ -1,1 +1,14 @@
 package data
+
+// EmptyStackErrMessage is message given when
+// there is illegal call caused by empty stack
+const EmptyStackErrMessage = "stack is empty"
+
+// Stack is type to process stack manner
+type Stack struct {
+}
+
+// Peek returns the top value of stack without removing it
+func (s *Stack) Peek() int {
+	panic(EmptyStackErrMessage)
+}
--- data/stack_test.go
+++ data/stack_test.go
@@ -1,1 +1,29 @@
 package data_test
+
+import (
+	"testing"
+
+	"github.com/Irainia/go-algorithm/data"
+)
+
+func TestStack(t *testing.T) {
+	t.Run("Peek", func(t *testing.T) {
+		t.Run("should panic if stack is empty", func(t *testing.T) {
+			stack := data.Stack{}
+
+			expectedMessage := data.EmptyStackErrMessage
+
+			defer func() {
+				if r := recover(); r != nil {
+					actualMessage := r.(string)
+
+					if actualMessage != expectedMessage {
+						t.Errorf("expected: %s -- actual: %s", expectedMessage, actualMessage)
+					}
+				}
+			}()
+			stack.Peek()
+			t.Error("expected: panic -- actual: not panic")
+		})
+	})
+}

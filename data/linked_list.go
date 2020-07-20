5c190764f23de2e1aca69dea33856a3207690417
Irainia <anwarhidayat14@gmail.com>
2020-07-20 08:12:59 +0700

    update to remove unnecessary implementation from linked list

--- data/linked_list.go
+++ data/linked_list.go
@@ -2,25 +2,6 @@ package data
 
 // LinkedList is the type to hold the list in a linked-list manner
 type LinkedList struct {
-	value int
-	count int
-
-	nextLinkedList *LinkedList
-}
-
-// GetLength gets the length of the current list
-func (l *LinkedList) GetLength() int {
-	return l.count
-}
-
-// NewEmptyLinkedList create empty linked-list
-func NewEmptyLinkedList() *LinkedList {
-	return &LinkedList{}
-}
-
-// NewAllocatedLinkedList create pre-allocated linked-list based on sequence
-func NewAllocatedLinkedList(sequence []int) *LinkedList {
-	return &LinkedList{
-		count: len(sequence),
-	}
+	Value          int
+	NextLinkedList *LinkedList
 }

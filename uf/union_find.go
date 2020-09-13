c353077f7c64b2f941921e276aa492c68fa80e31
Irainia <anwarhidayat14@gmail.com>
2020-09-13 14:59:03 +0700

    update union-find implementation to reduce n complexity

--- uf/union_find.go
+++ uf/union_find.go
@@ -1,56 +1,51 @@
 package uf
 
-// KeyManager manages key
-type KeyManager interface {
-	GetUniqueKey() int
-	GetLastKey() int
-	GetDefaultKey() int
-}
-
 // UnionFind is a type that performs union-find algorithm
 type UnionFind struct {
 	pointStorage []int
-	keyManager   KeyManager
+	pointWeight  []int
 }
 
 // Union unions two number points to form un-directed connection
 func (uf *UnionFind) Union(p, q int) {
-	newKey := uf.keyManager.GetUniqueKey()
-	if uf.pointStorage[p] == uf.pointStorage[q] {
-		if uf.pointStorage[p] == uf.keyManager.GetDefaultKey() {
-			uf.pointStorage[p] = newKey
-			uf.pointStorage[q] = newKey
-		}
-	} else {
-		if uf.pointStorage[p] == uf.keyManager.GetDefaultKey() {
-			uf.pointStorage[p] = uf.pointStorage[q]
-		} else if uf.pointStorage[q] == uf.keyManager.GetDefaultKey() {
-			uf.pointStorage[q] = uf.pointStorage[p]
+	rootP := uf.findRoot(p)
+	rootQ := uf.findRoot(q)
+	if rootP != rootQ {
+		if uf.pointWeight[rootP] >= uf.pointWeight[rootQ] {
+			uf.pointStorage[q] = rootP
+			uf.pointWeight[p] += uf.pointWeight[q]
 		} else {
-			keyForP := uf.pointStorage[p]
-			keyForQ := uf.pointStorage[q]
-			for i := 0; i < len(uf.pointStorage); i++ {
-				if uf.pointStorage[i] == keyForP ||
-					uf.pointStorage[i] == keyForQ {
-					uf.pointStorage[i] = newKey
-				}
-			}
+			uf.pointStorage[p] = rootQ
+			uf.pointWeight[q] += uf.pointWeight[p]
 		}
 	}
 }
 
 // IsConnected checks whether two number points are connected somehow
 func (uf *UnionFind) IsConnected(p, q int) bool {
-	if uf.pointStorage[p] == uf.pointStorage[q] {
-		return uf.pointStorage[p] != uf.keyManager.GetDefaultKey()
+	return uf.findRoot(p) == uf.findRoot(q)
+}
+
+func (uf *UnionFind) findRoot(i int) int {
+	for uf.pointStorage[i] != i {
+		uf.pointStorage[i] = uf.pointStorage[uf.pointStorage[i]]
+		i = uf.pointStorage[i]
 	}
-	return false
+	return i
 }
 
 // NewUnionFind initializes union-find
-func NewUnionFind(maximumNumber int, keyManager KeyManager) *UnionFind {
+func NewUnionFind(maximumNumber int) *UnionFind {
+	pointStorage := make([]int, maximumNumber)
+	for i := 0; i < maximumNumber; i++ {
+		pointStorage[i] = i
+	}
+	pointWeight := make([]int, maximumNumber)
+	for i := 0; i < maximumNumber; i++ {
+		pointWeight[i] = 1
+	}
 	return &UnionFind{
-		pointStorage: make([]int, maximumNumber),
-		keyManager:   keyManager,
+		pointStorage: pointStorage,
+		pointWeight:  pointWeight,
 	}
 }

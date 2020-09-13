030dc3636c67a80706bee41cb68b6f91f29868fe
Irainia <anwarhidayat14@gmail.com>
2020-09-13 12:37:23 +0700

    add play for union find

--- example/play_data.go
+++ example/play_data.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Irainia/go-algorithm/data"
+	"github.com/Irainia/go-algorithm/uf"
 )
 
 // PlayData plays all data structures
@@ -16,8 +17,64 @@ func PlayData() {
 	fmt.Println("... Stack ...")
 	playStack()
 
+	fmt.Println("... Union-Find ...")
+	playUnionFind()
+
 	fmt.Println("=== FINISH ===")
 	fmt.Println()
+}
+
+func playUnionFind() {
+	maximumNumber := 10
+	keyManager := &uf.DefaultKeyManager{}
+	unionFind := uf.NewUnionFind(maximumNumber, keyManager)
+
+	fmt.Println("... First Union ...")
+	firstPointNumber := [][]int{
+		[]int{4, 3},
+		[]int{3, 8},
+		[]int{6, 5},
+		[]int{9, 4},
+		[]int{2, 1},
+	}
+	for _, points := range firstPointNumber {
+		unionFind.Union(points[0], points[1])
+		fmt.Printf("union of [%d] and [%d]\n", points[0], points[1])
+	}
+	fmt.Println("... First Check ...")
+	firstCheckPoint := [][]int{
+		[]int{0, 7},
+		[]int{8, 9},
+	}
+	for _, points := range firstCheckPoint {
+		if unionFind.IsConnected(points[0], points[1]) {
+			fmt.Printf("[%d] and [%d] is connected\n", points[0], points[1])
+		} else {
+			fmt.Printf("[%d] and [%d] is not connected\n", points[0], points[1])
+		}
+	}
+	fmt.Println("... Second Union ...")
+	secondPointNumber := [][]int{
+		[]int{5, 0},
+		[]int{7, 2},
+		[]int{6, 1},
+		[]int{1, 0},
+	}
+	for _, points := range secondPointNumber {
+		unionFind.Union(points[0], points[1])
+		fmt.Printf("union of [%d] and [%d]\n", points[0], points[1])
+	}
+	fmt.Println("... Second Check ...")
+	secondCheckPoint := [][]int{
+		[]int{0, 7},
+	}
+	for _, points := range secondCheckPoint {
+		if unionFind.IsConnected(points[0], points[1]) {
+			fmt.Printf("[%d] and [%d] is connected\n", points[0], points[1])
+		} else {
+			fmt.Printf("[%d] and [%d] is not connected\n", points[0], points[1])
+		}
+	}
 }
 
 func playQueue() {
@@ -35,6 +92,7 @@ func playQueue() {
 	for queue.Length() > 0 {
 		fmt.Printf("dequeued: %d -- length: %d\n", queue.Dequeue(), queue.Length())
 	}
+	fmt.Println()
 }
 
 func playStack() {
@@ -52,4 +110,5 @@ func playStack() {
 	for stack.Length() > 0 {
 		fmt.Printf("popped: %d -- length: %d\n", stack.Pop(), stack.Length())
 	}
+	fmt.Println()
 }
